45b9e88b22c2eada078394155699f1bd30eb447d
Diogo Santos <diogofilipe1_santos@hotmail.com>
2020-10-27 08:29:41 +0000

    Alterei um pouco o esboco

--- safe_meetings/lib/screens/home.dart
+++ safe_meetings/lib/screens/home.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,29 +14,37 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green,
         actions: [
           IconButton(
+            icon: Icon(Icons.refresh), 
+            color: Colors.white,
+            onPressed: (){} ,// it should refresh the home screen
+            ),
+          IconButton(
             icon: Icon(Icons.search), 
             color: Colors.white,
-            onPressed: (){} // it should open the serach menu
+            onPressed: (){} // it should open the search menu
             )
             ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: GridView.count(
+        padding: EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
         children: [
           FlatButton(
             onPressed: () {}, 
             child: Center(child: Text('Conference 1')),
-            color: Colors.grey[200],
+            color: Colors.grey[300],
             ),
             FlatButton(
             onPressed: () {}, 
             child: Center(child: Text('Conference 2')),
-            color: Colors.grey[200],
+            color: Colors.grey[300],
             ),
             FlatButton(
             onPressed: () {}, 
             child: Center(child: Text('Conference 3')),
-            color: Colors.grey[200],
+            color: Colors.grey[300],
             )
         ],
         ),
