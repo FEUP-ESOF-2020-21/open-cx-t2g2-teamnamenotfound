441eb4ae94a88b0b507360729276e7db98755d8e
marinuas <marinadias1500@hotmail.com>
2020-12-06 16:42:56 +0000

    os organizadores conseguem ver o codigo da conferencia para a avaliar no see_info

--- safe_meetings/lib/screens/see_info.dart
+++ safe_meetings/lib/screens/see_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:safe_meetings/conference.dart';
+import 'package:safe_meetings/auth.dart';
 
 class SeeInfo extends StatefulWidget {
   @override
@@ -8,6 +9,7 @@ class SeeInfo extends StatefulWidget {
 
 class _SeeInfoState extends State<SeeInfo> {
   Conference conference;
+  Authentication auth = Authentication();
 
   var color;
 
@@ -27,9 +29,9 @@ class _SeeInfoState extends State<SeeInfo> {
         child: Column(children: [
           Text(conference.getName(),
               style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w600,
-                color: Colors.green[900]
+                  fontSize: 26,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.green[900]
               )),
         ]),
       ),
@@ -47,8 +49,8 @@ class _SeeInfoState extends State<SeeInfo> {
               width: 350,
               child: Text(conference.getDescription(),
                   style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.green[900]
+                      fontSize: 16,
+                      color: Colors.green[900]
                   )),
             ),
           ),
@@ -64,14 +66,14 @@ class _SeeInfoState extends State<SeeInfo> {
         child: Row(children: [
           Text("Date: ",
               style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
-                color: Colors.green[900]
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  color: Colors.green[900]
               )),
           Text(conference.getDate(),
               style: TextStyle(
-                fontSize: 16,
-                color: Colors.green[900]
+                  fontSize: 16,
+                  color: Colors.green[900]
               )),
         ]),
       ),
@@ -124,82 +126,137 @@ class _SeeInfoState extends State<SeeInfo> {
     this.selectStarColor(conference.getHygien());
     return Container(
         child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(children: [
-        Text("Hygien: ",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 18,
-              color: Colors.green[900]
-            )),
-        for (int i = 0; i < conference.getHygien(); i++)
-          Icon(Icons.star, color: this.color),
-      ]),
-    ));
+          padding: const EdgeInsets.all(16.0),
+          child: Row(children: [
+            Text("Hygien: ",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    color: Colors.green[900]
+                )),
+            for (int i = 0; i < conference.getHygien(); i++)
+              Icon(Icons.star, color: this.color),
+          ]),
+        ));
   }
 
   Widget securityWidget() {
     this.selectStarColor(conference.getSecurity());
     return Container(
         child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(children: [
-        Text("Security: ",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 18,
-              color: Colors.green[900]
-            )),
-        for (int i = 0; i < conference.getSecurity(); i++)
-          Icon(Icons.star, color: this.color),
-      ]),
-    ));
+          padding: const EdgeInsets.all(16.0),
+          child: Row(children: [
+            Text("Security: ",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    color: Colors.green[900]
+                )),
+            for (int i = 0; i < conference.getSecurity(); i++)
+              Icon(Icons.star, color: this.color),
+          ]),
+        ));
   }
 
   Widget interestWidget() {
     this.selectStarColor(conference.getInterest());
     return Container(
         child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(children: [
-        Text("Interest: ",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 18,
-              color: Colors.green[900]
-            )),
-        for (int i = 0; i < conference.getInterest(); i++)
-          Icon(Icons.star, color: this.color),
-      ]),
-    ));
+          padding: const EdgeInsets.all(16.0),
+          child: Row(children: [
+            Text("Interest: ",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    color: Colors.green[900]
+                )),
+            for (int i = 0; i < conference.getInterest(); i++)
+              Icon(Icons.star, color: this.color),
+          ]),
+        ));
+  }
+
+  Widget codeWidget() {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(children: [
+          Text("Code to Evaluate: ",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  color: Colors.green[900]
+              )),
+          Text(conference.getCode(),
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.green[900]
+              )),
+        ]),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     // gets the conferences passes by the loading screen
-    conference = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green[300],
-        title: Text('Safe Meetings', /*style: TextStyle(color: Colors.green[800]),*/),
-      ),
-      backgroundColor: Colors.white,
-      body: ListView(padding: const EdgeInsets.all(8.0), children: [
-        Container(
-          child: Column(
-                  children: [
-                    this.nameWidget(),
-                    this.descriptionWidget(),
-                    this.datewidget(),
-                    this.hourwidget(),
-                    this.localWidget(),
-                    this.hygienWidget(),
-                    this.securityWidget(),
-                    this.interestWidget(),
-            ],
+    conference = ModalRoute
+        .of(context)
+        .settings
+        .arguments;
+    String usermail = auth.getCurrentUser().email;
+    if (conference.isOrganizer(usermail)) {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green[300],
+            title: Text(
+              'Safe Meetings', /*style: TextStyle(color: Colors.green[800]),*/),
           ),
-        )
-      ]
-    ));
+          backgroundColor: Colors.white,
+          body: ListView(padding: const EdgeInsets.all(8.0), children: [
+            Container(
+              child: Column(
+                children: [
+                  this.nameWidget(),
+                  this.descriptionWidget(),
+                  this.datewidget(),
+                  this.hourwidget(),
+                  this.localWidget(),
+                  this.hygienWidget(),
+                  this.securityWidget(),
+                  this.interestWidget(),
+                  this.codeWidget(),
+                ],
+              ),
+            )
+          ]
+          ));
+    }
+    else {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green[300],
+            title: Text(
+              'Safe Meetings', /*style: TextStyle(color: Colors.green[800]),*/),
+          ),
+          backgroundColor: Colors.white,
+          body: ListView(padding: const EdgeInsets.all(8.0), children: [
+            Container(
+              child: Column(
+                children: [
+                  this.nameWidget(),
+                  this.descriptionWidget(),
+                  this.datewidget(),
+                  this.hourwidget(),
+                  this.localWidget(),
+                  this.hygienWidget(),
+                  this.securityWidget(),
+                  this.interestWidget(),
+                ],
+              ),
+            )
+          ]
+          ));
+    }
   }
 }
